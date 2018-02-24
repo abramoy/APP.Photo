1bc615e674408361525626dcd55408761a645fb7
Yevgeny Abramov <yevgeny.abramov2@mail.dcu.ie>
2018-02-24 19:30:54 +0000

    Adding form submission to send mail

--- src/main/java/com/photo/controllers/HelloController.java
+++ src/main/java/com/photo/controllers/HelloController.java
@@ -1,15 +1,33 @@
 package com.photo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.photo.model.Mail;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/")
-	public String helloWorld() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String helloWorld(Model model) {
+		model.addAttribute("mail", new Mail());
 		return "index";
 		
+	}
+	
+	@RequestMapping(value = "/sendMail", method = RequestMethod.POST)
+	public String update(@ModelAttribute Mail mail) {
+		
+		System.out.println(mail.getName());
+		System.out.println(mail.getEmail());
+		System.out.println(mail.getSubject());
+		System.out.println(mail.getMessage());
+	    
+
+	    return "index";
 	}
 
 }
---
+++ src/main/java/com/photo/model/Mail.java
@@ -0,0 +1,47 @@
+package com.photo.model;
+
+public class Mail {
+	
+	private String mailId;
+	
+	private String name;
+	
+	private String email;
+	
+	private String subject;
+	private String message;
+	
+	public String getMailId() {
+		return mailId;
+	}
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
